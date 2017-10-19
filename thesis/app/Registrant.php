7c731dc4a2fad8387d61163d7e57664776c07627
Dewi Anisa Istiaomah <dewianisaist@gmail.com>
2017-10-19 11:31:13 +0700

    add relationship table

--- thesis/app/Registrant.php
+++ thesis/app/Registrant.php
@@ -4,11 +4,26 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Registrant extends Eloquent
+class Registrant extends Model
 {
-    public function user() {
-        return $this->belongsTo('User');
+    
+    public function user()
+    {
+        return $this->hasOne('App\User');
     }
 
-    public $fillable = ['name'];
+    public function upload()
+    {
+        return $this->hasOne('App\Upload');
+    }
+
+    public function educations()
+    {
+        return $this->belongsToMany('App\Education', 'educational_background');
+    }
+
+    public function courses()
+    {
+        return $this->belongsToMany('App\Course', 'course_experience');
+    }
 }
