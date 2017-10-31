bbc59b9b22dcaec27ccca5e40098d61df83e1bd7
Dewi Anisa Istiaomah <dewianisaist@gmail.com>
2017-10-31 10:48:52 +0700

    manage models

--- thesis/app/Http/Models/Course.php
+++ thesis/app/Http/Models/Course.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     protected $table = 'courses';
+    public $fillable = ['major'];
     
     public function registrant() {
         return $this->belongsToMany('App\Http\Models\Registrant', 'course_experience');
