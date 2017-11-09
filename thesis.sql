3952dfbe36b95c50d96f0b0234d6f688d34a6de1
Dewi Anisa Istiaomah <dewianisaist@gmail.com>
2017-11-09 23:10:32 +0700

    fix typo table educational background

--- thesis.sql
+++ thesis.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 18, 2017 at 06:17 AM
+-- Generation Time: Nov 09, 2017 at 05:08 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.5
 
@@ -33,6 +33,14 @@ CREATE TABLE `courses` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `major`, `created_at`, `updated_at`) VALUES
+(1, 'Menjahit', '2017-10-27 22:10:54', '2017-10-27 22:10:54'),
+(2, 'Otomotif', '2017-10-27 22:11:04', '2017-10-27 22:11:04');
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +53,14 @@ CREATE TABLE `course_experience` (
   `organizer` varchar(100) DEFAULT NULL,
   `graduation_year` year(4) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `course_experience`
+--
+
+INSERT INTO `course_experience` (`registrant_id`, `course_id`, `organizer`, `graduation_year`) VALUES
+(1, 1, 'test penyelenggara 1', 2010),
+(1, 2, 'organizer 2', 2016);
 
 -- --------------------------------------------------------
 
@@ -69,15 +85,26 @@ CREATE TABLE `criterias` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `educational_backgroud`
+-- Table structure for table `educational_background`
 --
 
-CREATE TABLE `educational_backgroud` (
+CREATE TABLE `educational_background` (
   `registrant_id` int(11) NOT NULL,
   `education_id` int(11) NOT NULL,
   `name_institution` varchar(100) DEFAULT NULL,
   `graduation_year` year(4) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `educational_background`
+--
+
+INSERT INTO `educational_background` (`registrant_id`, `education_id`, `name_institution`, `graduation_year`) VALUES
+(1, 2, 'SDN 1 Bantul', 2004),
+(1, 3, 'SMP 1 Bantul', 2007),
+(1, 5, 'SMA 1 Bantul', 2010),
+(1, 8, 'UGM', 2014),
+(1, 9, 'UGM', 2017);
 
 -- --------------------------------------------------------
 
@@ -92,6 +119,19 @@ CREATE TABLE `educations` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `educations`
+--
+
+INSERT INTO `educations` (`id`, `stage`, `major`, `created_at`, `updated_at`) VALUES
+(2, 'SD', '', '2017-10-27 21:55:21', '2017-10-27 21:55:21'),
+(3, 'SMP', '', '2017-10-27 21:56:54', '2017-11-08 07:29:45'),
+(5, 'SMA', 'IPA', '2017-10-27 21:57:37', '2017-11-08 07:29:53'),
+(6, 'SMA', 'IPS', '2017-10-30 20:37:20', '2017-11-08 07:30:01'),
+(7, 'SMK', 'Multimedia', '2017-11-08 07:00:49', '2017-11-08 07:29:35'),
+(8, 'S1', 'Akutansi', '2017-11-08 19:59:02', '2017-11-09 09:06:02'),
+(9, 'S2', 'Manajemen', '2017-11-09 09:06:18', '2017-11-09 09:06:18');
 
 -- --------------------------------------------------------
 
@@ -180,7 +220,9 @@ INSERT INTO `permission_role` (`role_id`, `permission_id`) VALUES
 (3, 10),
 (3, 11),
 (3, 12),
-(5, 9);
+(5, 9),
+(8, 1),
+(9, 2);
 
 -- --------------------------------------------------------
 
@@ -193,18 +235,29 @@ CREATE TABLE `registrants` (
   `user_id` int(11) NOT NULL,
   `address` varchar(255) DEFAULT NULL,
   `phone_number` varchar(15) DEFAULT NULL,
-  `gender` tinyint(1) DEFAULT NULL COMMENT '1. Laki-laki\n2. Perempuan',
+  `gender` varchar(15) DEFAULT NULL,
   `place_birth` varchar(50) DEFAULT NULL,
   `date_birth` date DEFAULT NULL,
   `order_child` tinyint(2) DEFAULT NULL,
   `amount_sibling` tinyint(2) DEFAULT NULL,
-  `religion` tinyint(1) DEFAULT NULL COMMENT '1. Islam\n2. Katolik\n3. Kristen\n4. Hindu\n5. Budha\n6. Konghucu',
+  `religion` varchar(15) DEFAULT NULL,
   `biological_mother_name` varchar(100) DEFAULT NULL,
   `father_name` varchar(100) DEFAULT NULL,
   `parent_address` varchar(255) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `registrants`
+--
+
+INSERT INTO `registrants` (`id`, `user_id`, `address`, `phone_number`, `gender`, `place_birth`, `date_birth`, `order_child`, `amount_sibling`, `religion`, `biological_mother_name`, `father_name`, `parent_address`, `created_at`, `updated_at`) VALUES
+(1, 6, 'alamat abc', '1234567890', 'Perempuan', 'ttl abc', '1996-01-23', 4, 3, 'Konghucu', 'ibu abc', 'ayah abc', 'alamat ortu abc', NULL, '2017-11-05 23:52:53'),
+(2, 11, 'alamat', '1', '3', 'tempat', '2017-11-01', 2, 3, '1', 'ibu', 'ayah', 'alamat ortu', NULL, NULL),
+(3, 13, 'a', '1', '2', 'b', '2017-11-08', 1, 2, '3', 'c', 'd', 'e', NULL, NULL),
+(4, 5, 'alamat 3', '333333', 'Perempuan', 'ttl3', '2017-11-02', 3, 5, 'Kristen', 'ibu3', 'ayah3', 'ortu3', '2017-11-06 03:03:50', '2017-11-06 03:03:50'),
+(5, 17, 'Alamat 10', '0861010', 'Perempuan', 'ttl10', '1998-07-16', 2, 4, 'Budha', 'ibu 10', 'ayah 10', 'ortu 10', '2017-11-07 21:27:46', '2017-11-07 21:27:46');
 
 -- --------------------------------------------------------
 
@@ -256,7 +309,9 @@ INSERT INTO `roles` (`id`, `name`, `display_name`, `description`, `created_at`, 
 (1, 'superadmin', 'Superadmin', 'Superadmin merupakan peneliti', '2017-10-17 21:15:20', '2017-10-17 21:15:20'),
 (2, 'admin', 'Admin', 'Admin merupakan staf', '2017-10-17 21:15:46', '2017-10-17 21:15:46'),
 (3, 'kepala', 'Kepala BLK', 'Kepala merupakan pembuat keputusan', '2017-10-17 21:16:12', '2017-10-17 21:16:12'),
-(5, 'pendaftar', 'Pendaftar', 'Pendaftar merupakan calon peserta seleksi', '2017-10-17 21:17:32', '2017-10-17 21:17:32');
+(5, 'pendaftar', 'Pendaftar', 'Pendaftar merupakan calon peserta seleksi', '2017-10-17 21:17:32', '2017-10-25 11:13:56'),
+(8, 'a', 'a', 'a', '2017-10-25 11:18:18', '2017-10-25 11:18:18'),
+(9, 'b', 'b test', 'b', '2017-10-25 11:18:25', '2017-10-30 20:44:35');
 
 -- --------------------------------------------------------
 
@@ -274,7 +329,19 @@ CREATE TABLE `role_user` (
 --
 
 INSERT INTO `role_user` (`role_id`, `user_id`) VALUES
-(1, 1);
+(1, 1),
+(5, 5),
+(5, 6),
+(5, 9),
+(5, 11),
+(5, 13),
+(5, 14),
+(5, 15),
+(5, 16),
+(5, 17),
+(8, 8),
+(8, 10),
+(8, 12);
 
 -- --------------------------------------------------------
 
@@ -316,6 +383,19 @@ CREATE TABLE `sub_vocationals` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `sub_vocationals`
+--
+
+INSERT INTO `sub_vocationals` (`id`, `vocational_id`, `name`, `quota`, `long_training`, `goal`, `unit_competence`, `requirement_participant`, `final_registration_date`, `created_at`, `updated_at`) VALUES
+(2, 1, 'sub otomotif', 16, '100', 'test tujuan', 'unit', 'syarat', '2017-11-11 12:48:15', '2017-10-31 00:08:01', '2017-10-31 05:48:18'),
+(3, 4, 'sub test 1 edit', 30, '300', 'tujuan test edit', 'unit test edit', 'syarat test edit', '2017-11-30 16:59:59', '2017-10-31 00:23:17', '2017-10-31 00:30:40'),
+(4, 1, 'test 2', 10, '100', '10', '10', '10', '2017-11-02 16:11:58', '2017-10-31 01:01:46', '2017-10-31 05:29:37'),
+(5, 1, 'test 2', 10, '100', '10', '10', '01', '2017-11-11 12:11:29', '2017-10-31 02:05:31', '2017-10-31 05:28:15'),
+(6, 1, 'coba', 1, '1', '1', '1', '1', '2017-11-16 12:11:49', '2017-10-31 05:27:44', '2017-10-31 05:39:53'),
+(7, 5, 'coba 2', 1, '1', '1', '1q', '1', '2017-12-27 16:59:59', '2017-10-31 05:28:58', '2017-10-31 19:45:14'),
+(8, 4, 'te', 1, '1', '1', '1', '1', '2017-11-11 16:59:59', '2017-10-31 05:48:06', '2017-10-31 05:48:06');
+
 -- --------------------------------------------------------
 
 --
@@ -331,6 +411,17 @@ CREATE TABLE `uploads` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `uploads`
+--
+
+INSERT INTO `uploads` (`id`, `registrant_id`, `photo`, `ktp`, `last_certificate`, `created_at`, `updated_at`) VALUES
+(1, 1, 'photo_1679091c5a880faf6fb5e6087eb1b2dc.jpg', 'ktp_1679091c5a880faf6fb5e6087eb1b2dc.pdf', 'lastcertificate_1679091c5a880faf6fb5e6087eb1b2dc.pdf', NULL, '2017-11-07 21:25:11'),
+(2, 3, 'e', 'f', 'g', NULL, NULL),
+(5, 2, 'f', 'h', 'j', NULL, NULL),
+(6, 4, 'foto3', 'ktp3', 'ijazah3', '2017-11-06 03:03:50', '2017-11-06 03:03:50'),
+(7, 5, 'photo_70efdf2ec9b086079795c442636b55fb.jpg', 'ktp_70efdf2ec9b086079795c442636b55fb.pdf', 'lastcertificate_70efdf2ec9b086079795c442636b55fb.docx', '2017-11-07 21:27:46', '2017-11-07 21:30:13');
 
 -- --------------------------------------------------------
 
@@ -354,7 +445,19 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `identity_number`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, '12345678', 'Dewi Anisa Istiqomah', 'dewianisaist@gmail.com', '$2y$10$QSegh5oXdFAmkQ521SM8VO0s/.78UaRRsImJbn0aP/l52crOsnj3K', NULL, '2017-10-17 21:16:53', '2017-10-17 21:16:53');
+(1, '12345678', 'Dewi Anisa Istiqomah', 'dewianisaist@gmail.com', '$2y$10$2pNzr3yp2GYlVrqq5Wmfb.bRpA77/vfgf203DaENMV12dPqN9J0Yq', 'dBkSjmSqrt2YvYVy9kuzl3gwaCd0DiDiTo1l1mTBZH6YtRnH90uSmJjApKTW', '2017-10-17 21:16:53', '2017-11-01 23:46:58'),
+(5, '44444444', 'pendaftar 3', 'pendaftar3@tes.com', '$2y$10$a6rU.U6LUP531qy06kJuwuZFTJQ9F/QgagDsw01AAx9koPlhdtpJi', 'XREU371rNZXNgyyiZzk8fWStBE5YIp8KBWC6vDsrc8hZYmaaKTjYkrHVAvXw', '2017-10-17 23:27:11', '2017-11-06 03:04:08'),
+(6, '11111111', 'pendaftar 1abc', 'pendaftar1abc@tes.com', '$2y$10$OeonwX8NnwEcm2yX7.OJUukXUYWOdn/sPuANroPexuh4N.zlF/ojO', 'KpMg6HXComlbMuXljwz5xumqcxophHZy7YiVRC1CWcFG4WK91OyeYGZk4FOF', '2017-10-18 06:45:21', '2017-11-07 21:25:22'),
+(8, '77777777', 'z', 'ana@tes.comx', '$2y$10$49i22d1tkyUSUwYiTPmodORiK5zwJb247XG9AfpakJ4G6QXrXK70q', NULL, '2017-10-25 11:19:33', '2017-10-25 11:19:33'),
+(9, '55555555', 'b', 'b@tes.com', '$2y$10$kxjdNEVAZyF5/qN8gOYrM.STdJTGf1CLk7VniOORMwhIqtycRbryO', NULL, '2017-10-25 11:19:52', '2017-10-25 11:19:52'),
+(10, '88888888', 'a', 'f@tes.com', '$2y$10$GfMTtAXagRgRALezrb90iukwxlGDSGts96oV9KEKcCvr4yRA.9pte', NULL, '2017-10-25 11:20:11', '2017-10-25 11:48:04'),
+(11, '22222222', 'd', 'sinta@tes.com', '$2y$10$mdNfNiZPGkT7NIwBBelWReTb010bNp0G2psfLjlIBx9LX3UMZAiU6', 'NqWucyChEdDtUPFrk5dfAJfi5BhsbUwCbd1KLk3Ea3s8jTGQCwS63qMU0CoJ', '2017-10-25 12:36:00', '2017-11-06 03:04:23'),
+(12, '66666666', 'n', 'n@tes.com', '$2y$10$NXW.iTljrQPvVBnjW4.Lie7W29yMdWd3IGikT6i64dxRFvBWRq.1y', NULL, '2017-10-25 12:36:48', '2017-10-25 12:36:48'),
+(13, '33333333', 'm', 'm@tes.com', '$2y$10$49JH7QJLNw0uYfOFNofMXeka5vY8bPw6CVgI4qNHoaRsQeiQnrvyi', 'pPLjnFOF3ttRsUtx6YBj3eLvQhRtup3FAZXyI2zJZOxxQKmBh6jd8C2HBpD8', '2017-10-25 12:37:27', '2017-11-06 03:04:57'),
+(14, '0987654321', 'k', 'k@tes.com', '$2y$10$9vWduI9BkQxo.FnLxUcLFOeofM7WoGwlTsdt3v6yPgt9zl1LNIlNK', NULL, '2017-10-25 12:37:55', '2017-10-25 12:37:55'),
+(15, '567890', 'hello', 'hello@test.com', '$2y$10$YUl7GobZsmT2ZZzjUo5omuMthtcu/NFXbRgdMzu8SrSgsgsHciDUm', NULL, '2017-10-25 12:38:20', '2017-10-25 12:38:20'),
+(16, '123123', 'lala', 'la@tes.com', '$2y$10$WizBsbCIvCNzIyUdL7apy.npg24clkIcn0jQv78wsOS9gNLQkxZr.', '80NAWmTUfrr5wSBgSsC3Mwr7V7XyERsxlwE72qfeiRWvxDLwqfJu5i3WjSrk', '2017-10-25 13:03:24', '2017-10-25 13:05:24'),
+(17, '101010', 'pendaftar10', 'pendaftar10@tes.com', '$2y$10$IqshMWOtcLZ91WrA6eG04e4p7mLlJalpMboQmP90F0qmiH36/HjNG', NULL, '2017-11-07 21:26:19', '2017-11-07 21:26:19');
 
 -- --------------------------------------------------------
 
@@ -369,6 +472,15 @@ CREATE TABLE `vocationals` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `vocationals`
+--
+
+INSERT INTO `vocationals` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'Otomotif', 'otomotif', '2017-10-25 11:33:02', '2017-10-31 02:04:27'),
+(4, 'test 1', 'test 1', '2017-10-30 20:43:48', '2017-10-31 02:04:19'),
+(5, 'test 2', 'test 2', '2017-10-31 02:04:12', '2017-10-31 02:04:12');
 
 --
 -- Indexes for dumped tables
@@ -396,9 +508,9 @@ ALTER TABLE `criterias`
   ADD KEY `fk_criterias_criterias1_idx` (`group_criteria`);
 
 --
--- Indexes for table `educational_backgroud`
+-- Indexes for table `educational_background`
 --
-ALTER TABLE `educational_backgroud`
+ALTER TABLE `educational_background`
   ADD PRIMARY KEY (`registrant_id`,`education_id`),
   ADD KEY `fk_pendaftar_has_pendidikan_pendidikan1_idx` (`education_id`),
   ADD KEY `fk_pendaftar_has_pendidikan_pendaftar1_idx` (`registrant_id`);
@@ -512,22 +624,22 @@ ALTER TABLE `vocationals`
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `criterias`
 --
 ALTER TABLE `criterias`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `educational_backgroud`
+-- AUTO_INCREMENT for table `educational_background`
 --
-ALTER TABLE `educational_backgroud`
-  MODIFY `registrant_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `educational_background`
+  MODIFY `registrant_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `educations`
 --
 ALTER TABLE `educations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `permissions`
 --
@@ -537,7 +649,7 @@ ALTER TABLE `permissions`
 -- AUTO_INCREMENT for table `registrants`
 --
 ALTER TABLE `registrants`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `result_selections`
 --
@@ -547,7 +659,7 @@ ALTER TABLE `result_selections`
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `selections`
 --
@@ -557,22 +669,22 @@ ALTER TABLE `selections`
 -- AUTO_INCREMENT for table `sub_vocationals`
 --
 ALTER TABLE `sub_vocationals`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `uploads`
 --
 ALTER TABLE `uploads`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `vocationals`
 --
 ALTER TABLE `vocationals`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- Constraints for dumped tables
 --
@@ -591,9 +703,9 @@ ALTER TABLE `criterias`
   ADD CONSTRAINT `fk_criterias_criterias1` FOREIGN KEY (`group_criteria`) REFERENCES `criterias` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
--- Constraints for table `educational_backgroud`
+-- Constraints for table `educational_background`
 --
-ALTER TABLE `educational_backgroud`
+ALTER TABLE `educational_background`
   ADD CONSTRAINT `fk_pendaftar_has_pendidikan_pendaftar1` FOREIGN KEY (`registrant_id`) REFERENCES `registrants` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_pendaftar_has_pendidikan_pendidikan1` FOREIGN KEY (`education_id`) REFERENCES `educations` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
