aeaa95c58248cb87246ee201649ee730673d6ea6
Dewi Anisa Istiaomah <dewianisaist@gmail.com>
2018-01-23 11:15:07 +0700

    fix avatar pendaftar

--- thesis/resources/views/layouts/master_admin.blade.php
+++ thesis/resources/views/layouts/master_admin.blade.php
@@ -62,13 +62,13 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="../../avatars/avatar.png" class="user-image" alt="User Image">
+              <img src="../../../../avatars/avatar.png" class="user-image" alt="User Image">
               <span class="hidden-xs">{{ Auth::user()->name }}</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="../../avatars/avatar.png" class="img-circle" alt="User Image">
+                <img src="../../../..avatars/avatar.png" class="img-circle" alt="User Image">
                 
                 <p>
                  {{ Auth::user()->name }} 
@@ -98,7 +98,7 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="../../avatars/avatar.png" class="img-circle" alt="User Image">
+          <img src="../../../../avatars/avatar.png" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p>{{ Auth::user()->name }}</p>
