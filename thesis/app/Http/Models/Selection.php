9354df153e45bda9283a38c9d35aeac4d7981459
Dewi Anisa Istiaomah <dewianisaist@gmail.com>
2018-01-29 11:46:42 +0700

    fix model selection

--- thesis/app/Http/Models/Selection.php
+++ thesis/app/Http/Models/Selection.php
@@ -8,7 +8,7 @@ class Selection extends Model
 {
     protected $table = 'selections';
     
-    public $fillable = ['registrant_id', 'selection_schedule_id', 'written_value', 'interview_value'];
+    public $fillable = ['registration_id', 'selection_schedule_id', 'written_value', 'interview_value', 'ranking', 'status'];
 
     public function selectionschedule() {
         return $this->belongsTo('App\Http\Models\SelectionSchedule','selection_schedule_id');
