c6c0a5aa1db253a106dd3998caa943b8ad78b6d1
Dewi Anisa Istiaomah <dewianisaist@gmail.com>
2018-01-31 13:53:20 +0700

    add automate set selection schedule when register

--- thesis/app/Http/Controllers/RegistrationController.php
+++ thesis/app/Http/Controllers/RegistrationController.php
@@ -6,10 +6,10 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Models\Registration;
 use App\Http\Models\Subvocational;
-use App\Http\Models\Registrant;
 use App\Http\Models\User;
-use App\Http\Models\EducationalBackground;
-use DB;
+use App\Http\Models\Selection;
+use App\Http\Models\SelectionSchedule;
+// use App\Http\Models\EducationalBackground;
 use Auth;
 use Carbon;
 
@@ -57,10 +57,26 @@ class RegistrationController extends Controller
     */
    public function create()
    {
-       $date_now = Carbon\Carbon::now(7)->toDateTimeString();
-       $subvocational = Subvocational::where('final_registration_date', '>', $date_now)
-                                    ->lists('name','id');
-       return view('registration.create',compact('subvocational'));
+        $role_id = Auth::user()->roleId();
+        $user = User::with('registrant')->find(Auth::user()->id);
+        if ($role_id == 2) { 
+            $date_now = Carbon\Carbon::now(7)->toDateTimeString();
+            $subvocational = Subvocational::where('final_registration_date', '>', $date_now)
+                                            ->lists('name','id');
+            $selections = Selection::join('registrations', 'registrations.id', '=', 'selections.registration_id')
+                                    ->where('registrations.registrant_id', '=', $user->registrant->id)
+                                    ->where(function($query){
+                                        $query->whereNull('selections.status')
+                                              ->orWhere('selections.status', '=', '');
+                                    })
+                                    ->count();
+            if ($selections > 0) {
+                return redirect()->route('registration.index');
+            } else {
+                return view('registration.create',compact('subvocational'));
+            }
+        }
+        return redirect()->route('profile_users.show'); 
    }
 
    /**
@@ -71,17 +87,38 @@ class RegistrationController extends Controller
     */
    public function store(Request $request)
    {
-       $this->validate($request, [
-        'sub_vocational_id' => 'required',
-       ]);
+        $this->validate($request, [
+            'sub_vocational_id' => 'required',
+        ]);
 
-       $input = $request->all();
-       $user = User::with('registrant')->find(Auth::user()->id);
-       $input['registrant_id'] = $user->registrant->id;
-       $input['register_date'] = Carbon\Carbon::now(7)->toDateTimeString();
-       $registration = Registration::create($input);
+        $input = $request->all();
+        $user = User::with('registrant')->find(Auth::user()->id);
+        $input['registrant_id'] = $user->registrant->id;
+        $input['register_date'] = Carbon\Carbon::now(7)->toDateTimeString();
 
-       return redirect()->route('registration.index')
+        $selectionData = array();
+        $schedule = SelectionSchedule::whereSubVocationalId($input['sub_vocational_id'])
+                                    ->where('selection_schedules.date', '>', $input['register_date'])
+                                    ->orderBy('selection_schedules.date', 'ASC')
+                                    ->first();
+        
+        if ($schedule == null) {
+            return redirect()->route('registration.create')
+                            ->withInput($request->only('sub_vocational_id', 'remember'))
+                            ->withErrors([
+                                'sub_vocational_id' => "Silahkan hubungi Administrator. Jadwal seleksi belum tersedia."
+                            ]);
+        } else {
+            $selectionData['selection_schedule_id'] = $schedule->id;
+            $selectionData['create_at'] = $input['register_date'];
+            $selectionData['update_at'] = $input['register_date'];
+
+            $registration = Registration::create($input);
+
+            $selectionData['registration_id'] = $registration->id;
+            $selection = Selection::create($selectionData);
+            return redirect()->route('registration.index')
                         ->with('success','Selamat Anda berhasil melakukan pendaftaran pelatihan.');
+        }
    }
 }
